6de6152ca8b8ffaf324366c3077ebaa47ccc806f
Geo-Schon <dr-gudkov@mail.ru>
2023-02-18 19:44:12 +0400

    Перепутал папку)

---
+++ HW2.py
@@ -0,0 +1,5 @@
+summa = int (input ('Введите общее число журавликов: '))
+summ = summa / 6
+sereja = petya = round (summ)
+katya = round (4 * summ)
+print ('Петя сделал {}, Катя сделала {}, Сережа сделал {} журавликов'.format (petya, katya, sereja))
